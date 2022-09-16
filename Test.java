b767b0fc87f37a47680d8401a27c03520c89e463
sankisa <80066522+sankisa@users.noreply.github.com>
2022-09-16 16:48:09 -0700

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,10 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(10 + 20 + "RD");
+System.out.println("RD" + 10 + 20 );
+	}
+
+}
